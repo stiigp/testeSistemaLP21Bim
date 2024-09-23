c2d9273b64159734927a782ec7395c85a7703b01
EllaaManu <manucarmona0548@gmail.com>
2024-09-23 18:42:29 -0300

    patozinho all black

--- src/componentes/layouts/Pagina.jsx
+++ src/componentes/layouts/Pagina.jsx
@@ -1,17 +1,22 @@
 import Menu from "./Menu";
 import Cabecalho from "./Cabecalho";
 import { Container } from "react-bootstrap";
+import pato from '../../assets/imagens/pato.png';
+
+
 
 export default function Pagina(props) {
     return (
         <div className="bg-dark text-white p-3" >
         
         <Container className="bg-dark text-white p-3" >
-                <Cabecalho titulo="Sistema de controle Gerencial" />
+                <Cabecalho titulo="˖ ࣪✦ Loot Ledger ‧₊˚𓂃 ." />
                 <Menu />
                 {
                     props.children
                 }
+                
+                <img src={pato} />
             </Container>
         </div>
 
