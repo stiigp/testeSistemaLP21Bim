fef1ebbfa0b809f248fa95f5764d5466734dd1bc
EllaaManu <manucarmona0548@gmail.com>
2024-09-25 20:11:05 -0300

    eita

---
+++ src/componentes/Telas/Formularios/FormCadFornecedor.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import {
+  Row,
+  Col,
+  Form,
+  Button
+} from "react-bootstrap";
+
+export default function FormCadFornecedores(props) {
+
+    const [formValidado, setFormValidado] = useState(false);
+    const fornecedorReseta = {
+        codigo:0,
+        nome:"",
+        cnpj: "",
+        contato: "",
+        localizacao: ""
+    }
+
+    function manipularSubmissao(evento){
+        const form = evento.currentTarget;
+        if (form.checkValidity()){
+            if(!props.modoEdicao){
+            props.setListaFornecedor([...props.listaFornecedor, props.fornecedorSelecionado]);
+            
+           
+            window.alert("Fornecedor inserido com sucesso!");
+            props.setExibirFornecedor(true);
+        } else{
+            props.setListaFornecedor(props.listaFornecedor.map((item) =>
+              item.codigo === props.fornecedorSelecionado.codigo ? props.fornecedorSelecionado
+             : item
+            ));
+            window.alert("fornecedor atualizado com sucesso!");
+            props.setFornecedorSelecionado(fornecedorReseta);
+            props.setModoEdicao(false);
+            props.setExibirFornecedor(true);
+          }
+        }
+        else{
+            setFormValidado(true);
+        }
+        evento.preventDefault();
+        evento.stopPropagation();
+      }
+
+      function manipularMudanca(evento){
+        const elemento = evento.target.name;
+        const valor    = evento.target.value; 
+        props.setFornecedorSelecionado({...props.fornecedorSelecionado, [elemento]:valor});
+    }
+    return (
+
+        <Form noValidate validated={formValidado} onSubmit={manipularSubmissao}>
+            <Row>
+            <Col>
+              {/* ########## Código ########## */}
+              <Form.Group className="mb-3">
+                <Form.Label>Código</Form.Label>
+                <Form.Control disabled={props.modoEdicao} required type="number" id="codigo" name="codigo" value={props.fornecedorSelecionado.codigo} onChange={manipularMudanca} placeholder="Código" />
+                <Form.Control.Feedback type="invalid">
+                  Por favor, informe o código do fornecedor!
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+            <Col>
+          {/* ########## nome ########## */}
+          <Form.Group className="mb-3">
+            <Form.Label>Nome:</Form.Label>
+            <Form.Control required type="text" id="nome" name="nome" value={props.fornecedorSelecionado.descricao} onChange={manipularMudanca} placeholder="Nome:" />
+            <Form.Control.Feedback type="invalid">
+              Por favor, informe a nome do fonecedor!
+            </Form.Control.Feedback>
+          </Form.Group>
+            </Col>
+            </Row>
+            <Row>
+            <Col>
+            {/* ########## cnpj ########## */}
+            <Form.Group className="mb-3">
+                <Form.Label>CNPJ:</Form.Label>
+                <Form.Control required type="text" id="cnpj" name="cnpj" value={props.fornecedorSelecionado.cnpj} onChange={manipularMudanca} placeholder="CNPJ:" />
+                <Form.Control.Feedback type="invalid">
+                Por favor, informe o CNPJ do fornecedor!
+                </Form.Control.Feedback>
+            </Form.Group>
+            </Col>
+            
+            <Col>
+            {/* ########## localizacao ########## */}
+            <Form.Group className="mb-3">
+                <Form.Label>Localizacao:</Form.Label>
+                <Form.Control required type="text" id="localizacao" name="localizacao" value={props.fornecedorSelecionado.localizacao} onChange={manipularMudanca} placeholder="Localizacao:" />
+                <Form.Control.Feedback type="invalid">
+                Por favor, informe o localizacao do fornecedor!
+                </Form.Control.Feedback>
+            </Form.Group>
+            </Col>
+            </Row>
+
+                    <Row className="mt-2 mb-2">
+                <Col md={1}>
+                <Button id="botao" type="submit" variant="success">
+                    Confirmar
+                </Button>{" "}
+                </Col>
+                <Col md={{ offset: 1 }}>
+                <Button
+                    onClick={() => {
+                    props.setExibirFornecedor(true);
+                    }}
+                    type="button"
+                    variant="success"
+                >
+                    Voltar
+                </Button>
+                </Col>
+            </Row>
+            </Form>
+
+        
+    );  
+}
---
+++ src/dados/mockFornecedores.js
@@ -0,0 +1,30 @@
+export const fornecedores = [
+    {
+        "codigo": 1,
+        "nome": "Geek Power Supplies",
+        "cnpj": "12.345.678/0001-90",
+        "contato": "(11) 98765-4321",
+        "localizacao": "Distrito Tecnológico, São Paulo - SP"
+    },
+    {
+        "codigo": 2,
+        "nome": "Nerd Station Merch",
+        "cnpj": "98.765.432/0001-01",
+        "contato": "(21) 99876-5432",
+        "localizacao": "Av. Otaku, 42 - Tokyo Village, Curitiba - PR"
+    },
+    {
+        "codigo": 3,
+        "nome": "Pixel Figures",
+        "cnpj": "34.567.890/0001-12",
+        "contato": "(31) 91234-5678",
+        "localizacao": "Rua dos Pixels, 256 - Geekland, Rio de Janeiro - RJ"
+    },
+    {
+        "codigo": 4,
+        "nome": "Tech Hero Gadgets",
+        "cnpj": "76.543.210/0001-23",
+        "contato": "(41) 92345-6789",
+        "localizacao": "Rua das Estrelas, 789 - Galáxia Nerd, Belo Horizonte - MG"
+    }
+];
